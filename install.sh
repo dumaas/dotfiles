7464f3c95202cfecf336b6048e97810bd53c83fa
dumaas <christiangonzalezblack@gmail.com>
2025-07-27 08:40:26 -0400

    test update install script

--- install.sh
+++ install.sh
@@ -6,8 +6,13 @@ cp ./.gitmessage ~
 cp ./.gitconfig ~
 git config --global commit.template ~/.gitmessage
 
-# oh-my-zsh & plugins
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh || true
+# Install zsh
+apt-get install -y zsh
+
+# Install oh-my-zsh with Powerlevel10k theme
+zsh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.2.1/zsh-in-docker.sh)"
+
+# Install plugins
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
