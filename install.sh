ebb61ff28ba52e56acdf8b61222c3da9b29fee1a
dumaas <christiangonzalezblack@gmail.com>
2025-07-27 14:46:24 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ cp ./.zshrc ~
 cp ./.p10k.zsh ~
 
 # Provide app-specific configurations to zshrc
-if [ -f ~/.devcontainer/.shell_override.sh ]; then
-    echo "shell_override.sh found, appending to .zshrc"
+if [ -f ~/.shell_override.sh ]; then
+    echo "shell override file found, appending to .zshrc"
     echo "" >> ~/.zshrc  # Add blank line for separation
-    cat ~/.devcontainer/.shell_override.sh >> ~/.zshrc
+    cat ~/.shell_override.sh >> ~/.zshrc
 fi
