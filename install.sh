c57851fcafcbf42009a0228e242ec5a7310714fb
dumaas <christiangonzalezblack@gmail.com>
2025-07-27 14:37:00 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -12,19 +12,11 @@ zsh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.
 # Install plugins
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
+cp ./.zshrc ~
 cp ./.p10k.zsh ~
 
-# Backup existing .zshrc if it exists
-if [ -f ~/.zshrc ]; then
-    cp ~/.zshrc ~/.zshrc.backup
-fi
-
-# Copy your dotfiles .zshrc to home directory
-cp ./.zshrc ~
-
-# If backup exists, append it to the end of the new .zshrc
-if [ -f ~/.zshrc.backup ]; then
+# Provide app-specific configurations to zshrc
+if [ -f ~/.shell_override.sh ]; then
     echo "" >> ~/.zshrc  # Add blank line for separation
-    cat ~/.zshrc.backup >> ~/.zshrc
-    rm ~/.zshrc.backup  # Clean up backup file
+    cat ~/.shell_override.sh >> ~/.zshrc
 fi
