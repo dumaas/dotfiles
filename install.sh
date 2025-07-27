d49a3ecf2d47b18220f28cf5efd0f1d76e8defe4
dumaas <christiangonzalezblack@gmail.com>
2025-07-27 08:46:47 -0400

    add p10k to dotfiles

--- install.sh
+++ install.sh
@@ -13,3 +13,4 @@ zsh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.
 zsh -c 'git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions'
 zsh -c 'git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting'
 cp ./.zshrc ~
+cp ./.p10k.zsh ~
